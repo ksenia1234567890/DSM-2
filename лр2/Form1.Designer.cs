6d264d742e70c794758c656c0f9bbbb9cc332a49
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2023-03-27 21:24:32 +0300

    Добавьте файлы проекта.

---
+++ лр2/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace лр2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(290, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button2.Location = new System.Drawing.Point(462, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Рандом";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 83);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(290, 238);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(326, 50);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(29, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "*нажмите на одну из кнопок, чтобы \r\n  получить массив автоматически";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(28, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите массив через пробел";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(54, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите число для поиска";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(290, 446);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(326, 50);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(124, 446);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Результат поиска";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button7.Location = new System.Drawing.Point(290, 296);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(154, 46);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Последовательный поиск";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(462, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 46);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Бинарный поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button4.Location = new System.Drawing.Point(290, 348);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 47);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Минимальное";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button5.Location = new System.Drawing.Point(462, 348);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 47);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Максимальное";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 548);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Поиск";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ лр2/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace лр2
+// Необходимо реализовать программу, которая позволит производить   поиск по одномерному массиву
+
+// Функционал должен удовлетворять следующим условиям:
+
+// массив может быть введен с клавиатуры;
+// сгенерирован случайно;
+// считан с файла;
+// пользователь может ввести число для поиска числа в массиве;
+// программа может вывести минимальное или максимальное значение массива по запросу пользователя;
+// результат поиска выводится как на экран, так и в файл с соответствующим текстом;
+// пользователь может выбрать вариант поиска по массиву.
+
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[] array = new int[10]; //массив, в котором производится поиск числа, минимального или максимального значения
+        
+        // Считывание массива из файла
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string way = "D:\\\\3 курс\\6 семестр\\рпм\\массив.txt";
+            string line = File.ReadAllText(way);
+            int y = 0;
+            foreach (var i in line.Split('*'))
+            {
+                array[y] = int.Parse(i);
+                y++;
+            }
+            string outcome = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                outcome += Convert.ToString(array[i]) + " ";
+            }
+            textBox1.Text = $"{outcome}";
+        }
+        
+        // Заполнение массива случайными числами
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            Random numeric = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = numeric.Next(1, 100);
+                line += Convert.ToString(array[i]) + " ";
+            }
+
+            textBox1.Text = $"{line}";
+        }
+        // Считывание массива из текстового поля
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string line = textBox2.Text;
+                int j = 0;
+                foreach (var i in line.Split(' '))
+                {
+                    array[j] = Convert.ToInt32(i);
+                    j++;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат. Попробуйте ввести заново");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Последовательный поиск. Поиск числа в массиве
+            try
+            {
+                int number = int.Parse(textBox2.Text);
+                int y = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (number == array[i])
+                    {
+                        MessageBox.Show($"Введённое число присутствует в массиве. Позиция (0 не является первым элементом) - {i+1}");
+                        y = 1;
+                        FileStream fs = new FileStream("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt", FileMode.Append);
+                        StreamWriter sw = new StreamWriter(fs);
+                        sw.WriteLine($"Введённое число присутствует в массиве. Позиция (0 не является первым элементом) - {i+1}");
+                        sw.Close();
+                        fs.Close();
+                        break;
+                    }
+                }
+                if (y == 0)
+                {
+                    MessageBox.Show($"Число {number} отсутствует в массиве.");
+                    FileStream fs = new FileStream("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt", FileMode.Append);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine($"Число {number} отсутствует в массиве.");
+                    sw.Close();
+                    fs.Close();
+                }
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Неверный формат. Попробуйте ввести заново");
+            }
+        }
+        // Создание документа, где будут фиксироваться все действия с массивом
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            File.Create("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt");
+        }
+
+        // Сортировка пузырьком
+        public int[] Sort(int[] copy)
+        {
+            int count = 0;
+            for (int i = 1; ; i++)
+            {
+                if (i == copy.Length)
+                {
+                    i = 1;
+                }
+                int y = i - 1;
+                if (copy[y] < copy[i])
+                {
+                    count++;
+                }
+                if (copy[y] > copy[i])
+                {
+                    int changer = copy[i];
+                    copy[i] = copy[y];
+                    copy[y] = changer;
+                    count = 0;
+                }
+                if (count == array.Length)
+                {
+                    break;
+                }
+            }
+            return copy;
+        }
+
+        // Бинарный поиск: пользователь нажал на кнопку "Бинарный поиск"
+        // Сначала производится сортировка, после - бинарный поиск
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Сортировка пузырьком
+
+                // копия массива
+                int[] copy = new int[array.Length]; 
+                array.CopyTo(copy, 0);
+                // сортировка копии массива
+                Sort(copy); 
+                string copy_str = String.Join(" ", copy);
+                MessageBox.Show($"Отсортированный массив\n{copy_str}");
+                int number = int.Parse(textBox2.Text);
+                // результат поиска
+                int search = BinarySearch(copy, number); 
+
+                // Если результат поиска равен -122, вывести сообщение и записать в файл "Заданное число не найдено"
+                // Иначе вывести сообщение с числом и полученным результатом
+                if (search == -122)
+                {
+                    textBox3.Text = $"Число {number}  не найдено";
+                    FileStream fs = new FileStream("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt", FileMode.Append);
+                    StreamWriter sw2 = new StreamWriter(fs);
+                    sw2.WriteLine(textBox3.Text);
+                    sw2.Close();
+                    fs.Close();
+                }
+                else
+                {
+                    textBox3.Text = $"{search} = {number}. Заданное число найдено";
+                    FileStream fs = new FileStream("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt", FileMode.Append);
+                    StreamWriter sw2 = new StreamWriter(fs);
+                    sw2.WriteLine(textBox3.Text);
+                    sw2.Close();
+                    fs.Close();
+                }
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Неверный формат. Попробуйте ввести заново");
+            }
+
+        }
+        // Метод BinarySearch производит бинарный поиск
+
+        public int BinarySearch(int[] copy, int number)
+        {
+            // Нижняя и верхняя границы
+            int lower = 0;
+            int upper = copy.Length - 1;
+            
+            // Цикл: пока нижняя граница меньше верхней или равна, цикл уменьшает или увеличивает границу
+            while (lower <= upper)
+            {
+                // Середина массива
+                var middle = (lower + upper) / 2;
+                MessageBox.Show($"{middle}");
+
+                if (number == copy[middle])
+                {
+                    return copy[middle];
+                }
+
+                if (number < copy[middle])
+                {
+                    // Верхняя граница
+                    upper = middle - 1;
+                }
+                else
+                {
+                    // Нижняя граница
+                    lower = middle + 1;
+                }
+            }
+            // Число не найдено
+            return -122;
+
+        }
+        // Вывод минимального значения
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //копия массива
+            int[] copy = new int[array.Length]; 
+            array.CopyTo(copy, 0);
+            // сортировка копии массива
+            Sort(copy); 
+            textBox3.Text = $"Минимальное число в массиве: {copy[0]}";
+            FileStream fs = new FileStream("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt", FileMode.Append);
+            StreamWriter sw2 = new StreamWriter(fs);
+            sw2.WriteLine(textBox3.Text);
+            sw2.Close();
+            fs.Close();
+        }
+        // Вывод максимального значения
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //копия массива
+            int[] copy = new int[array.Length]; 
+            array.CopyTo(copy, 0);
+            // сортировка копии массива
+            Sort(copy); 
+            textBox3.Text = $"Максимальное число в массиве: {copy[copy.Length-1]}";
+            FileStream fs = new FileStream("D:\\\\3 курс\\6 семестр\\рпм\\лр2.txt", FileMode.Append);
+            StreamWriter sw2 = new StreamWriter(fs);
+            sw2.WriteLine(textBox3.Text);
+            sw2.Close();
+            fs.Close();
+        }
+    }
+}
